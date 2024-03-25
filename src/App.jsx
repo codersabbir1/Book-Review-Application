88ba656a19951bd05f9c7b2c47cb0ac621d3d141
fokhrul2029 <ifokhrul.2029@gmail.com>
2024-03-25 21:37:17 +0600

    setup the project stractured

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container mx-auto px-4">
+        <h1 className="text-3xl font-bold text-purple-600">Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
