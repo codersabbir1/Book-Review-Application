54561216cabc6cf82f8227ddc958690e0ba24825
fokhrul2029 <ifokhrul.2029@gmail.com>
2024-03-28 21:54:25 +0600

    update drop down to sort on book list page

--- src/components/BookContainer/BookContainer.jsx
+++ src/components/BookContainer/BookContainer.jsx
@@ -1,13 +1,6 @@
 import TabContents from "../TabContents/TabContents";
 
 function BookContainer() {
-  const options = [
-    { id: 1, item: "Sort By", selected: true },
-    { id: 2, item: "Rating", selected: false },
-    { id: 3, item: "Number of pages", selected: false },
-    { id: 4, item: "Publisher year", selected: false },
-  ];
-
   return (
     <div className="py-10">
       <div className="container mx-auto px-4 ">
@@ -15,27 +8,13 @@ function BookContainer() {
           <h1 className="text-3xl text-center  font-bold">Books</h1>
         </div>
         <div className="workSans py-6 flex justify-center items-center">
-          <select className="select select-success bg-success text-white text-base font-normal ">
-            {options.map((item) => (
-              <option
-                key={item.id}
-                className={`${
-                  item.selected
-                    ? "bg-white text-slate-400"
-                    : "bg-white text-slate-700"
-                }`}
-                disabled="false"
-                selected="false"
-              >
-                {item.item}
-              </option>
-            ))}
-            {/* <option className="bg-white " disabled selected>
+          <select className="select select-success bg-success text-white text-base font-normal">
+            <option className="bg-white text-slate-300" defaultValue>
               Sort By
             </option>
             <option className="bg-white text-slate-700">Rating</option>
             <option className="bg-white text-slate-700">Number of pages</option>
-            <option className="bg-white text-slate-700">Publisher year</option> */}
+            <option className="bg-white text-slate-700">Publisher year</option>
           </select>
         </div>
         <TabContents />
