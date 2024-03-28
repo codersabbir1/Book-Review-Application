8b2dd2aca957aa28e902307edd704d7326537568
fokhrul2029 <ifokhrul.2029@gmail.com>
2024-03-28 19:38:00 +0600

    added a custom barchart on read pages

---
+++ src/components/CustomChart/CustomChart.jsx
@@ -0,0 +1,71 @@
+/* eslint-disable react/prop-types */
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from "recharts";
+
+const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+
+const data = [
+  {
+    name: "Read List Book's Page",
+    uv: 8000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Wish List Book's Page",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+];
+
+const getPath = (x, y, width, height) => {
+  return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${
+    y + height / 3
+  }
+  ${x + width / 2}, ${y}
+  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
+    x + width
+  }, ${y + height}
+  Z`;
+};
+
+const TriangleBar = (props) => {
+  const { fill, x, y, width, height } = props;
+
+  return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+};
+
+export default function CustomChart() {
+  return (
+    <div className="py-20 px-5 rounded-lg bg-gray-200">
+      <BarChart
+        width={1000}
+        height={400}
+        data={data}
+        margin={{
+          top: 20,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Bar
+          dataKey="uv"
+          fill="#8884d8"
+          shape={<TriangleBar />}
+          label={{ position: "top" }}
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+          ))}
+        </Bar>
+      </BarChart>
+    </div>
+  );
+}
+
+CustomChart.demoUrl =
+  "https://codesandbox.io/s/bar-chart-with-customized-shape-dusth";
--- src/pages/ReadPages/ReadPages.jsx
+++ src/pages/ReadPages/ReadPages.jsx
@@ -1,10 +1,11 @@
+import CustomChart from "../../components/CustomChart/CustomChart";
+
 function ReadPages() {
   return (
-    <div className="container mx-auto px-4">
-      <h1 className="text-3xl py-10 text-center">Hello ReadPages Component</h1>
+    <div className="container mx-auto px-4 py-10 text-center flex justify-center items-center">
+      <CustomChart />
     </div>
   );
 }
 
 export default ReadPages;
- 
